06413359fe434effc98afcd9212df1e39d0d4cc0
zikglobal <125207118+zikglobal@users.noreply.github.com>
2023-10-04 22:12:43 +0100

    Add project files.

---
+++ Hospitals.Utitities/WebSiteRoles.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospitals.Utitities
+{
+    public static class WebSiteRoles
+    {
+        public const string WebSite_Admin = "Admin";
+        public const string WebSite_Patient = "Patient";
+        public const string WebSite_Doctor = "Doctor";
+
+    }
+}
